d2a168c3205c83397a9340c10def435e069b5893
Sebastian Gonzales Abad <sgabad@hotmail.com>
2021-04-29 20:37:01 -0500

    Se agregaron Resources, verificar si añadir mas

--- PERUSTARS/PERUSTARS/Resources/PersonResource.cs
+++ PERUSTARS/PERUSTARS/Resources/PersonResource.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace PERUSTARS.Domain.Models
+namespace PERUSTARS.Resources
 {
-    public class Person
+    public class PersonResource
     {
+        public long Id { get; set; }
         public string Firstname { get; set; }
         public string Lastname { get; set; }
 
-        public long Id { get; set; };
     }
 }
