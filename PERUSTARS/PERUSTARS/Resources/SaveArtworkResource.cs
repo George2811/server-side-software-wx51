a4dfb6723ac56c0f97594b2d7c2c376f668d98d7
Sebastian Gonzales Abad <sgabad@hotmail.com>
2021-04-30 16:11:11 -0500

    Se agrego SaveResources

---
+++ PERUSTARS/PERUSTARS/Resources/SaveArtworkResource.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace PERUSTARS.Resources
+{
+
+    public class SaveArtworkResource
+    {
+        [Required]
+        [MaxLength(100)]
+        public string ArtTitle { get; set; }
+
+        [Required]
+        [MaxLength(250)]
+        public string ArtDescription { get; set; }
+
+
+    }
+}
---
+++ PERUSTARS/PERUSTARS/Resources/SaveEventResource.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace PERUSTARS.Resources
+{
+    public class SaveEventResource
+    {
+        [Required]
+        [MaxLength(100)]
+        public string EventTitle { get; set; }
+
+
+        [Required]
+        [MaxLength(25)]
+        public string EventType { get; set; }
+
+        [Required]
+        [DataType(DataType.DateTime)]
+        public DateTime DateStart { get; set; }
+
+        [Required]
+        [DataType(DataType.DateTime)]
+        public DateTime DateEnd { get; set; }
+
+        [Required]
+        [MaxLength(250)]
+        public string EventDescription { get; set; }
+
+       
+    }
+}
