2f79e6e5a0622e304fb48e2a19840c9da45695b4
Sebastian Gonzales Abad <sgabad@hotmail.com>
2021-05-01 19:57:31 -0500

    Se agregaron Controllers

---
+++ PERUSTARS/PERUSTARS/Controllers/FollowersController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PERUSTARS.Domain.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PERUSTARS.Controllers
+{
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    [ApiController]
+    public class FollowersController : ControllerBase
+    {
+        private readonly IFollowerService _followerService;
+
+        public FollowersController(IFollowerService followerService)
+        {
+            _followerService = followerService;
+        }
+    }
+}
---
+++ PERUSTARS/PERUSTARS/Controllers/HobbyistController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PERUSTARS.Domain.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PERUSTARS.Controllers
+{
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    [ApiController]
+    public class HobbyistController : ControllerBase
+    {
+        private readonly IHobbyistService _hobbyistService;
+
+        public HobbyistController(IHobbyistService hobbyistService)
+        {
+            _hobbyistService = hobbyistService;
+        }
+    }
+}
