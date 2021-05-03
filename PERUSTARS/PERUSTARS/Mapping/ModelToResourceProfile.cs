ecf0b196009092e73bb0ac384cc75da24a2eab46
Sebastian Gonzales Abad <sgabad@hotmail.com>
2021-05-03 18:49:49 -0500

    Se añadio Mappings

---
+++ PERUSTARS/PERUSTARS/Mapping/ModelToResourceProfile.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using AutoMapper;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PERUSTARS.Domain.Models;
+using PERUSTARS.Resources;
+
+namespace PERUSTARS.Mapping
+{
+    public class ModelToResourceProfile : Profile
+    {
+        public ModelToResourceProfile() {
+            CreateMap<Artist, ArtistResource>();
+            CreateMap<Hobbyist, Hobbyist>();
+            CreateMap<Person, PersonResource>();
+            CreateMap<Event, EventResource>();
+            CreateMap<ClaimTicket, ClaimTicketResource>();
+
+
+
+        }
+
+    }
+}
