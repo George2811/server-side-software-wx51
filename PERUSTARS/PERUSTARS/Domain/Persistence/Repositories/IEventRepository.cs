e5209d576d81a77e883347d9a51758aea4c40782
Sebastian Gonzales Abad <sgabad@hotmail.com>
2021-05-02 21:24:21 -0500

    Se agrego Repositories

--- PERUSTARS/PERUSTARS/Domain/Persistence/Repositories/IEventRepository.cs
+++ PERUSTARS/PERUSTARS/Domain/Persistence/Repositories/IEventRepository.cs
@@ -9,7 +9,7 @@ namespace PERUSTARS.Domain.Persistence.Repositories
     public interface IEventRepository
     {
         Task<IEnumerable<Event>> ListAsync();
-        Task<IEnumerable<Event>> ListByArtistIdAsync(long eventId);
+        Task<IEnumerable<Event>> ListByArtistIdAsync(long artistId);
         Task AddAsync(Event _event);
         Task<Event> FindById(long id);
         void Update(Event _event);
---
+++ PERUSTARS/PERUSTARS/Persistence/Repositories/ArtworkRepository.cs
@@ -0,0 +1,51 @@
+﻿using PERUSTARS.Domain.Persistence.Repositories;
+using PERUSTARS.Domain.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PERUSTARS.Domain.Models;
+
+namespace PERUSTARS.Persistence.Repositories
+{
+    public class ArtworkRepository : BaseRepository, IArtworkRepository
+    {
+        public ArtworkRepository(AppDbContext context) : base(context)
+        {
+        }
+
+        public async Task AddAsync(Artwork artwork)
+        {
+            await _context.Artworks.AddAsync(artwork);
+        }
+
+        public async Task<Artwork> FindById(long artworkId)
+        {
+            return await _context.Artworks.FindAsync(artworkId);
+        }
+
+        public async Task<IEnumerable<Artwork>> ListAsync()
+        {
+            return await _context.Artworks.ToListAsync();
+        }
+
+        public async Task<IEnumerable<Artwork>> ListByArtistIdAsync(long artistId) 
+        {
+
+            return await _context.Artworks
+                  .Where(pt => pt.ArtistId == artistId)
+                  .ToListAsync();
+        }
+
+        public void Remove(Artwork artwork)
+        {
+            _context.Artworks.Remove(artwork);
+        }
+
+        public void Update(Artwork artwork)
+        {
+            _context.Artworks.Update(artwork);
+        }
+    }
+}
