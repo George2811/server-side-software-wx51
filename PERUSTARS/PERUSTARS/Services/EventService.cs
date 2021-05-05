831b0b4b8c367bd6023068a086c78c6a350a7580
Sebastian Hernandez <shernandez1131@gmail.com>
2021-05-05 18:53:25 -0500

    Revisar por favor

---
+++ PERUSTARS/PERUSTARS/Services/EventService.cs
@@ -0,0 +1,105 @@
+using PERUSTARS.Domain.Models;
+using PERUSTARS.Domain.Persistence.Repositories;
+using PERUSTARS.Domain.Services;
+using PERUSTARS.Domain.Services.Communications;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PERUSTARS.Services
+{
+    public class EventService : IEventService
+    {
+        private readonly IEventRepository _eventRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public async Task<EventResponse> DeleteAsync(long id)
+        {
+            var existingEvent = await _eventRepository.FindById(id);
+
+            if (existingEvent == null)
+                return new EventResponse("Event not found");
+
+            try
+            {
+                _eventRepository.Remove(existingEvent);
+                await _unitOfWork.CompleteAsync();
+
+                return new EventResponse(existingEvent);
+            }
+            catch (Exception ex)
+            {
+                return new EventResponse($"An error ocurred while deleting the Event: {ex.Message}");
+            }
+        }
+
+        public async Task<EventResponse> GetByIdAsync(long id)
+        {
+            var existingEvent = await _eventRepository.FindById(id);
+
+            if (existingEvent == null)
+                return new EventResponse("Event not found");
+            return new EventResponse(existingEvent);
+        }
+
+        public async Task<IEnumerable<Event>> ListAsync()
+        {
+            return await _eventRepository.ListAsync();
+        }
+
+        public async Task<IEnumerable<Event>> ListAsyncByArtistId(long Id)
+        {
+            return await _eventRepository.ListByArtistIdAsync(Id);
+        }
+
+        public Task<IEnumerable<Event>> ListAsyncByEventType(ETypeOfEvent eTypeOf)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<EventResponse> SaveAsync(Event _event)
+        {
+            
+            try
+            {
+                await _eventRepository.AddAsync(_event);
+                await _unitOfWork.CompleteAsync();
+
+                return new EventResponse(_event);
+            }
+            catch (Exception ex)
+            {
+                return new EventResponse($"An error ocurred while saving the event: {ex.Message}");
+            }
+        
+        }
+
+        public async Task<EventResponse> UpdateAsync(long id, Event _event)
+        {
+            var existingEvent = await _eventRepository.FindById(id);
+
+            if (existingEvent == null)
+                return new EventResponse("Event not found");
+
+            existingEvent.EventTitle = _event.EventTitle;
+            existingEvent.EventType = _event.EventType;
+            existingEvent.DateStart = _event.DateStart;
+            existingEvent.DateEnd = _event.DateEnd;
+            existingEvent.EventDescription = _event.EventDescription;
+            existingEvent.EventAditionalInfo = _event.EventAditionalInfo;
+
+            try
+            {
+                _eventRepository.Update(existingEvent);
+                await _unitOfWork.CompleteAsync();
+
+                return new EventResponse(existingEvent);
+            }
+            catch (Exception ex)
+            {
+                return new EventResponse($"An error ocurred while updating the Event: {ex.Message}");
+            }
+        }
+    }
+}
